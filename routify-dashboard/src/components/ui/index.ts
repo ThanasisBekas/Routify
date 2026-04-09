f33c6d964a7523bee5ca216602e108a3740b0807
Athanasios Bekas <v-abekas@eurobank.gr>
2026-04-10 00:00:00 +0300

    fix: update CorrelationIdGatewayFilterFactory to use RoutifyHeaders directly and remove deprecated constants

--- routify-dashboard/src/components/ui/index.ts
+++ routify-dashboard/src/components/ui/index.ts
@@ -7,21 +7,15 @@
  * ```
  */
 export { Button } from './Button'
-export type { ButtonProps } from './Button'
 
 export { Input, Textarea } from './Input'
-export type { InputProps, TextareaProps } from './Input'
 
 export { Modal, ModalHeader, ModalBody, ModalFooter } from './Modal'
-export type { ModalProps } from './Modal'
 
 export { Badge } from './Badge'
-export type { BadgeProps } from './Badge'
 
 export { Card } from './Card'
-export type { CardProps } from './Card'
 
 export { Table } from './Table'
 
 export { Select } from './Select'
-export type { SelectOption } from './Select'
